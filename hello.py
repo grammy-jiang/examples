65c8bfd6d789ae2071cc83a29e920b9906d68015
Grammy Jiang <grammy.jiang@gmail.com>
2021-03-17 20:56:22 +1100

    Black the code

--- hello.py
+++ hello.py
@@ -1,13 +1,18 @@
 import os
 import sys
+from typing import List, Tuple
 
 from django.conf import settings
+from django.conf.urls import url
+from django.core.handlers.wsgi import WSGIHandler, WSGIRequest
+from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse
 
-DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+DEBUG: bool = os.environ.get("DEBUG", "on") == "on"
 
-SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+SECRET_KEY: str = os.environ.get("SECRET_KEY", os.urandom(32))
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS: List[str] = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 
 settings.configure(
     DEBUG=DEBUG,
@@ -15,27 +20,21 @@ settings.configure(
     ALLOWED_HOSTS=ALLOWED_HOSTS,
     ROOT_URLCONF=__name__,
     MIDDLEWARE_CLASSES=(
-        'django.middleware.common.CommonMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        "django.middleware.common.CommonMiddleware",
+        "django.middleware.csrf.CsrfViewMiddleware",
+        "django.middleware.clickjacking.XFrameOptionsMiddleware",
     ),
 )
 
-from django.conf.urls import url
-from django.core.wsgi import get_wsgi_application
-from django.http import HttpResponse
+
+def index(request: WSGIRequest) -> HttpResponse:
+    return HttpResponse("Hello World")
 
 
-def index(request):
-    return HttpResponse('Hello World')
+urlpatterns: Tuple = (url(r"^$", index),)
 
 
-urlpatterns = (
-    url(r'^$', index),
-)
-
-
-application = get_wsgi_application()
+application: WSGIHandler = get_wsgi_application()
 
 
 if __name__ == "__main__":
